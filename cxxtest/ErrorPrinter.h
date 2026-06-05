cb8ea267be092436fec55190f1ffee194d433d84
Matthew Jimenez <matthew.jimenez@outlook.com>
2026-06-04 20:26:10 -0500

    Make various cxxtest printers not depend on std

--- cxxtest/ErrorPrinter.h
+++ cxxtest/ErrorPrinter.h
@@ -16,34 +16,29 @@
 // The ErrorPrinter is a simple TestListener that
 // just prints "OK" if everything goes well, otherwise
 // reports the error in the format of compiler messages.
-// The ErrorPrinter uses std::cout
+// The ErrorPrinter uses std::cout or stdout
 //
 
-#include <cxxtest/Flags.h>
-
-#ifndef _CXXTEST_HAVE_STD
-#   define _CXXTEST_HAVE_STD
-#endif // _CXXTEST_HAVE_STD
-
 #include <cxxtest/ErrorFormatter.h>
-#include <cxxtest/StdValueTraits.h>
-
-#ifdef _CXXTEST_OLD_STD
-#   include <iostream.h>
-#else // !_CXXTEST_OLD_STD
-#   include <iostream>
-#endif // _CXXTEST_OLD_STD
 
 namespace CxxTest
 {
 class ErrorPrinter : public ErrorFormatter
 {
 public:
+#ifdef _CXXTEST_HAVE_STD
     ErrorPrinter(CXXTEST_STD(ostream) &o = CXXTEST_STD(cout), const char *preLine = ":", const char *postLine = "",
                  const char *errorString = "Error",
                  const char *warningString = "Warning") :
         ErrorFormatter(new StdOStreamAdapter(o), preLine, postLine, errorString, warningString) {}
+#else
+    ErrorPrinter(FILE *o = stdout, const char *preLine = ":", const char *postLine = "",
+                 const char *errorString = "Error",
+                 const char *warningString = "Warning") :
+        ErrorFormatter(new StdioFileAdapter(o), preLine, postLine, errorString, warningString) {}
+#endif // _CXXTEST_HAVE_STD
 
+    virtual ~ErrorPrinter() { delete outputStream(); }
 };
 }
 
